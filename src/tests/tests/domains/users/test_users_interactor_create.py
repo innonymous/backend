588cf3da8f0b9670dbd47e3e3167177d5314f870
DOCtorActoAntohich <fairy666death@mail.ru>
2023-05-06 18:16:18 +0300

    add users interactor create tests

---
+++ src/tests/tests/domains/users/test_users_interactor_create.py
@@ -0,0 +1,40 @@
+import pytest
+from pytest_mock import MockFixture
+
+from innonymous.data.repositories.users import UsersRepository
+from innonymous.domains.users.errors import UsersAlreadyExistsError
+from innonymous.domains.users.entities import UserEntity, UserCredentialsEntity
+from innonymous.domains.users.interactors import UsersInteractor
+
+from .conftest import UserEntityProtocol
+
+
+@pytest.mark.user
+class TestUsersInteractorCreateMethod:
+    async def test_successful(
+        self, mocker: MockFixture, user_entity_factory: UserEntityProtocol, users_repository: UsersRepository
+    ) -> None:
+        expected_user = user_entity_factory()
+        credentials = UserCredentialsEntity(alias=expected_user.alias, password="password")
+
+        def check_if_alias_matches(created_user: UserEntity) -> None:
+            assert expected_user.alias == created_user.alias
+
+        mocker.patch.object(users_repository, "create", side_effect=check_if_alias_matches)
+
+        interactor = UsersInteractor(users_repository)
+        actual_user = await interactor.create(credentials)
+
+        assert expected_user.alias == actual_user.alias
+
+    async def test_user_already_exists(
+        self, mocker: MockFixture, user_entity_factory: UserEntityProtocol, users_repository: UsersRepository
+    ) -> None:
+        credentials = UserCredentialsEntity(alias="duplicate", password="password")
+
+        mocker.patch.object(users_repository, "create", side_effect=UsersAlreadyExistsError())
+
+        interactor = UsersInteractor(users_repository)
+
+        with pytest.raises(UsersAlreadyExistsError):
+            _ = await interactor.create(credentials)
