a9d6908d5457e40f71bc050e6379f1da39b125a1
DOCtorActoAntohich <fairy666death@mail.ru>
2023-05-06 18:47:30 +0300

    add a couple more get tests to cover more scenarios

--- src/tests/tests/domains/users/test_users_interactor_get.py
+++ src/tests/tests/domains/users/test_users_interactor_get.py
@@ -2,8 +2,9 @@ import pytest
 from pytest_mock import MockFixture
 
 from innonymous.data.repositories.users import UsersRepository
-from innonymous.domains.users.errors import UsersError
+from innonymous.domains.users.errors import UsersError, UsersNotFoundError
 from innonymous.domains.users.interactors import UsersInteractor
+from innonymous.domains.users.entities import UserCredentialsEntity
 
 from .conftest import UserEntityProtocol
 
@@ -11,55 +12,95 @@ from .conftest import UserEntityProtocol
 @pytest.mark.user
 class TestUsersInteractorGetMethod:
     async def test_by_id(
-            self,
-            mocker: MockFixture,
-            user_entity_factory: UserEntityProtocol,
-            user_repository: UsersRepository
+        self, mocker: MockFixture, user_entity_factory: UserEntityProtocol, users_repository: UsersRepository
     ) -> None:
         expected_user = user_entity_factory()
-        mocker.patch.object(user_repository, "get", return_value=expected_user)
+        mocker.patch.object(users_repository, "get", return_value=expected_user)
 
-        interactor = UsersInteractor(user_repository)
+        interactor = UsersInteractor(users_repository)
 
         actual_user = await interactor.get(id_=expected_user.id)
         assert expected_user == actual_user
 
     async def test_by_alias(
-            self,
-            mocker: MockFixture,
-            user_entity_factory: UserEntityProtocol,
-            user_repository: UsersRepository
+        self, mocker: MockFixture, user_entity_factory: UserEntityProtocol, users_repository: UsersRepository
     ) -> None:
         expected_user = user_entity_factory()
-        mocker.patch.object(user_repository, "get", return_value=expected_user)
+        mocker.patch.object(users_repository, "get", return_value=expected_user)
 
-        interactor = UsersInteractor(user_repository)
+        interactor = UsersInteractor(users_repository)
 
         actual_user = await interactor.get(alias=expected_user.alias)
         assert expected_user == actual_user
 
-    async def test_fails_with_no_arguments(
-            self,
-            mocker: MockFixture,
-            user_repository: UsersRepository
-    ) -> None:
-        mocker.patch.object(user_repository, "get", side_effect=UsersError())
+    async def test_by_credentials(
+        self,
+        mocker: MockFixture,
+        user_entity_factory: UserEntityProtocol,
+        user_credentials: UserCredentialsEntity,
+        users_repository: UsersRepository,
+    ):
+        mocker.patch.object(users_repository, "create")
 
-        interactor = UsersInteractor(user_repository)
+        interactor = UsersInteractor(users_repository)
+        expected_user = await interactor.create(user_credentials)
+
+        mocker.patch.object(users_repository, "get", return_value=expected_user)
+
+        actual_user = await interactor.get(credentials=user_credentials)
+        assert expected_user == actual_user
+
+    async def test_fails_with_no_arguments(self, mocker: MockFixture, users_repository: UsersRepository) -> None:
+        mocker.patch.object(users_repository, "get", side_effect=UsersError())
+
+        interactor = UsersInteractor(users_repository)
 
         with pytest.raises(UsersError):
             _ = await interactor.get()
 
     async def test_id_conflicts_with_alias(
-            self,
-            mocker: MockFixture,
-            user_entity_factory: UserEntityProtocol,
-            user_repository: UsersRepository
+        self, mocker: MockFixture, user_entity_factory: UserEntityProtocol, users_repository: UsersRepository
     ) -> None:
         expected_user = user_entity_factory()
-        mocker.patch.object(user_repository, "get", side_effect=UsersError())
+        mocker.patch.object(users_repository, "get", side_effect=UsersError())
 
-        interactor = UsersInteractor(user_repository)
+        interactor = UsersInteractor(users_repository)
 
         with pytest.raises(UsersError):
             _ = await interactor.get(id_=expected_user.id, alias=expected_user.alias)
+
+    async def test_id_conflicts_with_credentials(
+        self,
+        mocker: MockFixture,
+        user_entity_factory: UserEntityProtocol,
+        user_credentials: UserCredentialsEntity,
+        users_repository: UsersRepository,
+    ) -> None:
+        expected_user = user_entity_factory()
+        mocker.patch.object(users_repository, "get", side_effect=UsersError())
+
+        interactor = UsersInteractor(users_repository)
+
+        with pytest.raises(UsersError):
+            _ = await interactor.get(id_=expected_user.id, credentials=user_credentials)
+
+    async def test_user_not_found(
+        self,
+        mocker: MockFixture,
+        user_entity_factory: UserEntityProtocol,
+        user_credentials: UserCredentialsEntity,
+        users_repository: UsersRepository,
+    ) -> None:
+        mocker.patch.object(users_repository, "get", return_value=None)
+
+        some_user = user_entity_factory()
+        interactor = UsersInteractor(users_repository)
+
+        tasks_to_fail = [
+            interactor.get(id_=some_user.id),
+            interactor.get(alias=some_user.alias),
+            interactor.get(credentials=user_credentials),
+        ]
+        for task in tasks_to_fail:
+            with pytest.raises(UsersNotFoundError):
+                await task
